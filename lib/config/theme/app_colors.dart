4f53e3b74823b441d0bbc44fbb4556a56e4c60fa
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-13 12:36:33 -0500

    se eligio los colores de la app

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Claro
+  static const primaryLight = Color.fromARGB(255, 43, 168, 182);
+  static const backgroundLight = Colors.white;
+  static const textLight = Colors.black;
+
+  // Oscuro
+  static const primaryDark = Color.fromARGB(255, 21, 58, 63);
+  static const backgroundDark = Color(0xFF121212);
+  static const textDark = Colors.white;
+}
