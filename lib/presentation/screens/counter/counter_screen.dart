97dbcddc852a6614ce432cac0c0d2dd386d7b8b6
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-16 19:26:00 -0500

    theme provider con riverpod terminado, los colores del tema ya se pueden escoger (implementado con un statenotifier)

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -11,14 +11,14 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
-    final bool isDarkMode = ref.watch(isDarkModeProvider);
+    final isDarkMode = ref.watch(themeNotifierProvider).isDarkMode;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Counter Screen"),
         actions: [
           IconButton(
             onPressed: () {
-              ref.read(isDarkModeProvider.notifier).state = !isDarkMode;
+              ref.read(themeNotifierProvider.notifier).toggleDarkMode();
             },
             icon: (isDarkMode)
                 ? Icon(Icons.dark_mode_outlined)
